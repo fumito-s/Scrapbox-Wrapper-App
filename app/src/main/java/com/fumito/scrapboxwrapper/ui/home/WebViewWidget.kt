733edcc5597ab71d0987a160a67514b6a6d7d326
fumito-s <fmt.fumito@gmail.com>
2022-03-07 23:03:33 +0900

    Update WebViewWidget size

--- app/src/main/java/com/fumito/scrapboxwrapper/ui/home/WebViewWidget.kt
+++ app/src/main/java/com/fumito/scrapboxwrapper/ui/home/WebViewWidget.kt
@@ -4,10 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.viewinterop.AndroidView
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 
 @Composable
 fun WebViewWidget() {
     AndroidView(
+        modifier = Modifier.fillMaxSize(),
         factory = { WebView(it) },
         update = { webView ->
             webView.webViewClient = WebViewClient()
