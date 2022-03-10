6b74844f5bb160958164bd9e672d1b5364c583de
fumito-s <fmt.fumito@gmail.com>
2022-03-10 22:38:20 +0900

    Handle back button pressed

--- app/src/main/java/com/fumito/scrapboxwrapper/ui/home/WebViewWidget.kt
+++ app/src/main/java/com/fumito/scrapboxwrapper/ui/home/WebViewWidget.kt
@@ -4,26 +4,46 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.viewinterop.AndroidView
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.google.accompanist.insets.statusBarsPadding
 
 @Composable
 fun WebViewWidget() {
+    val backPressed = remember { mutableStateOf(false) }
     AndroidView(
         modifier = Modifier
             .fillMaxSize()
             .statusBarsPadding(),
-        factory = { WebView(it) },
-        update = { webView ->
-            webView.webViewClient = WebViewClient()
-            webView.apply {
+        factory = {
+            WebView(it).apply {
+                webViewClient = WebViewClient()
                 settings.apply {
                     javaScriptEnabled = true
                     domStorageEnabled = true
+
+                }
+            }.also { webView ->
+                webView.loadUrl("https://scrapbox.io/fumito/")
+            }
+        },
+        update = { webView ->
+            if (backPressed.value) {
+                backPressed.value = false
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    // close app
                 }
             }
-            webView.loadUrl("https://scrapbox.io/fumito/")
         }
+    )
+
+    BackHandler(
+        enabled = true,
+        onBack = { backPressed.value = true }
     )
 }
